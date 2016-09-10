e05953e394d41f7da455fe882dad8e456a85db14
Mahmud Hasan <biumahmud2012@gmail.com>
2016-09-10 15:47:08 +0600

    edit login activity and create forget activity

--- app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/MainActivity.java
+++ app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/MainActivity.java
@@ -1,7 +1,6 @@
 package bd.edu.httpdaffodilvarsity.jobtrack;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
---
+++ app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/ui/login/ChangePassword.java
@@ -0,0 +1,15 @@
+package bd.edu.httpdaffodilvarsity.jobtrack.ui.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import bd.edu.httpdaffodilvarsity.jobtrack.R;
+
+public class ChangePassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}

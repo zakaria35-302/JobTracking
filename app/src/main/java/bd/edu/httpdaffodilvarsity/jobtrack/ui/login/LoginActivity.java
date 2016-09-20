6f59e25e2113020ee6a75b7609a4d2e474765dac
Mahmud Hasan <biumahmud2012@gmail.com>
2016-09-20 14:02:08 +0600

    successfully completed change password activity design

--- app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/ui/login/LoginActivity.java
+++ app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/ui/login/LoginActivity.java
@@ -2,10 +2,11 @@ package bd.edu.httpdaffodilvarsity.jobtrack.ui.login;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import bd.edu.httpdaffodilvarsity.jobtrack.R;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

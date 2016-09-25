fcd33f079dd3ae60d1059cb351d1c27573c8896a
Mahmud Hasan <biumahmud2012@gmail.com>
2016-09-25 14:07:23 +0600

    add new account form

--- app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/ui/login/CreateUserAccount.java
+++ app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/ui/login/CreateUserAccount.java
@@ -4,14 +4,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import bd.edu.httpdaffodilvarsity.jobtrack.R;
 
 public class CreateUserAccount extends Activity {
+
+    Spinner spinnerEmployeeGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user_account);
+
+        spinnerEmployeeGender = (Spinner) findViewById(R.id.spinner_employee_gender);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.employee_gender, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // Apply the adapter to the spinner
+        spinnerEmployeeGender.setAdapter(adapter);
     }
 
     public  void  GoToLoginActivity(View v){

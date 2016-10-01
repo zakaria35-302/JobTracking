6cc884a79404f7d954f04f79c99aec16bdb007a6
Mahmud Hasan <biumahmud2012@gmail.com>
2016-10-01 13:06:00 +0600

    add Employee profile Screen in android Studio

---
+++ app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/ui/login/EditEmployeeAccount.java
@@ -0,0 +1,14 @@
+package bd.edu.httpdaffodilvarsity.jobtrack.ui.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+import bd.edu.httpdaffodilvarsity.jobtrack.R;
+
+public class EditEmployeeAccount extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_employee_account);
+    }
+}

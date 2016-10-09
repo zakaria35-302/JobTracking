4bd578e01fd7c175ea9eb1fc5aed34b479d9e482
Muhammad Zakaria <zakariam32@gmail.com>
2016-10-09 12:14:01 +0600

    Tab Activity-Profiel,Task Management,Job for every Profile

---
+++ app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/TaskManagement.java
@@ -0,0 +1,13 @@
+package bd.edu.httpdaffodilvarsity.jobtrack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TaskManagement extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_management);
+    }
+}

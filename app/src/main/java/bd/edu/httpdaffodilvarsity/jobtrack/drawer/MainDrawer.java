4df7c2c8593001a234b5573ad0dd7a215d3a7bef
Mahmud Hasan <biumahmud2012@gmail.com>
2016-10-30 10:33:54 +0600

    edit drawer activity

--- app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/drawer/MainDrawer.java
+++ app/src/main/java/bd/edu/httpdaffodilvarsity/jobtrack/drawer/MainDrawer.java
@@ -1,8 +1,12 @@
 package bd.edu.httpdaffodilvarsity.jobtrack.drawer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,8 +16,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
+import bd.edu.httpdaffodilvarsity.jobtrack.JobManagement.JobManagement;
 import bd.edu.httpdaffodilvarsity.jobtrack.R;
+import bd.edu.httpdaffodilvarsity.jobtrack.ui.login.EditEmployeeAccount;
+import bd.edu.httpdaffodilvarsity.jobtrack.ui.login.HomeSettingTab;
+import bd.edu.httpdaffodilvarsity.jobtrack.ui.login.HomeTabActivity;
+import bd.edu.httpdaffodilvarsity.jobtrack.ui.login.HomeTaskManagement;
+import bd.edu.httpdaffodilvarsity.jobtrack.ui.login.Home_Admin_Tab;
+import bd.edu.httpdaffodilvarsity.jobtrack.ui.login.ProfileViewActivity;
+import bd.edu.httpdaffodilvarsity.jobtrack.ui.login.TaskManagement;
 
 public class MainDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -70,6 +83,8 @@ public class MainDrawer extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            //Intent intent = new Intent(this, HomeSettingTab.class);
+            //startActivity(intent);
             return true;
         }
 
@@ -80,19 +95,46 @@ public class MainDrawer extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        /*SearchView searchView = (SearchView)menu.findItem(R.id.nav_camera).getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));*/
+
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            //View rootViewEmployeeTaskManagement = inflater.inflate(R.layout.activity_home_task_management, container, false);
+            final MenuItem btnEmployeeTaskManagement = (MenuItem) findViewById(R.id.nav_camera);
+            btnEmployeeTaskManagement.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    Intent intent = new Intent(MainDrawer.this, ProfileViewActivity.class);
+                    MainDrawer.this.startActivity(intent);
+                    return true;
+                }
+            });
+
+           /* Intent intent = new Intent(MainDrawer.this, ProfileViewActivity.class);
+            startActivity(intent);*/
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(MainDrawer.this, HomeTaskManagement.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_slideshow) {
+            Intent intent = new Intent(MainDrawer.this, JobManagement.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_manage) {
+            Intent intent = new Intent(MainDrawer.this, HomeSettingTab.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_share) {
+            Intent intent = new Intent(MainDrawer.this, Home_Admin_Tab.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_send) {
+            Intent intent = new Intent(MainDrawer.this, JobManagement.class);
+            startActivity(intent);
 
         }
 
